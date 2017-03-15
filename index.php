0bd2fe9986fd6548f1565c89aa8b47e182a6cc4d
Raivk <thomas.lbrn76@gmail.com>
2017-03-15 22:20:40 +0100

    base files for project

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>LE JEU DE L'ESPACE INTERSTELLAIRE</title>
+        <link tpye="text/css" rel="stylesheet" href="css/pure-min.css"/>
+        <link type="text/css" rel="stylesheet" href="css/index.css"/>
+    </head>
+    <body>
+        <h1 class="big_title center_me">LE JEU DE L'ESPACE INTERSTELLAIRE</h1>
+        <h2 class="center_me"><small class="small_things">(avec des planetes)</small></h2>
+    </body>
+</html>
